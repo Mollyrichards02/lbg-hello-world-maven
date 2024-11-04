1729778d8027ef74a122840fd1afe604ed640fda
mollyrichards02 <mollyrichards02@hotmail.co.uk>
2024-11-04 13:59:21 +0000

    added new name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Molly"));
+        System.out.println(sayHelloToSomeone("Ela"));
         System.out.println(sayGoodbye());
     }
 
