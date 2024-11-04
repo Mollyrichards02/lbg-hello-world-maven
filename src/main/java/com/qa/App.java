47d6bcc2649a5ae056d581583570d7013781192c
mollyrichards02 <mollyrichards02@hotmail.co.uk>
2024-11-04 13:57:48 +0000

    added new name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Molly"));
+        System.out.println(sayHelloToSomeone("Jack"));
         System.out.println(sayGoodbye());
     }
 
